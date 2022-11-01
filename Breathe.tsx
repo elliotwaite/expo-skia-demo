6d366160013cba5437db358207ae5cb87e483990
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2022-11-01 13:30:15 -0700

    Add `R` as a dependency

--- Breathe.tsx
+++ Breathe.tsx
@@ -15,7 +15,6 @@ import {
   mix,
 } from '@shopify/react-native-skia';
 
-const NUM_RINGS = 16
 const c1 = "#61bea2";
 const c2 = "#529ca0";
 
@@ -26,21 +25,21 @@ interface RingProps {
 
 const Ring = ({ index, progress }: RingProps) => {
   const { width, height } = useWindowDimensions();
-  const R = width / 16;
+  const R = width / 4;
   const center = useMemo(
     () => vec(width / 2, height / 2 - 64),
     [height, width]
   );
 
-  const theta = (index * (2 * Math.PI)) / NUM_RINGS;
+  const theta = (index * (2 * Math.PI)) / 6;
   const transform = useComputedValue(() => {
     const { x, y } = polar2Canvas(
       { theta, radius: progress.current * R },
       { x: 0, y: 0 }
     );
     const scale = mix(progress.current, 0.3, 1);
-    return [{ translateX: x * 4 }, { translateY: y * 4 }, { scale }];
-  }, [progress]);
+    return [{ translateX: x }, { translateY: y }, { scale }];
+  }, [progress, R]);
 
   return (
     <Group origin={center} transform={transform}>
@@ -71,7 +70,7 @@ export const Breathe = () => {
       <Fill color="rgb(36,43,56)" />
       <Group origin={center} transform={transform} blendMode="screen">
         <BlurMask style="solid" blur={40} />
-        {new Array(NUM_RINGS).fill(0).map((_, index) => {
+        {new Array(6).fill(0).map((_, index) => {
           return <Ring key={index} index={index} progress={progress} />;
         })}
       </Group>
