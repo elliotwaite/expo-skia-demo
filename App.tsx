ffcf05c4ca61847e8030d69d50f563c80c6fd9bf
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2022-11-01 13:42:30 -0700

    Create a simplified demo

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <WithSkiaWeb
-        getComponent={() => import('./Breathe')}
+        getComponent={() => import('./HelloWorld')}
         fallback={<Text style={{ textAlign: 'center' }}>Loading Skia...</Text>}
       />
     </View>
