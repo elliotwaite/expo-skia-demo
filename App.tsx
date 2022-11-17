b8fffd14175ded0401ad3b80189e23b733f8e5fd
elliotwaite <1767836+elliotwaite@users.noreply.github.com>
2022-11-17 11:40:48 -0800

    Add PathExample.tsx

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <WithSkiaWeb
         // @ts-ignore
-        getComponent={() => import('./HelloWorld')}
+        getComponent={() => import('./PathExample')}
         fallback={<Text style={{ textAlign: 'center' }}>Loading Skia...</Text>}
       />
     </View>
---
+++ PathExample.tsx
@@ -0,0 +1,9 @@
+import {Canvas, Path} from "@shopify/react-native-skia"
+
+export default function PathExample() {
+  return (
+    <Canvas style={{ flex: 1 }}>
+      <Path path="M 100 200 L 158 100 L 216 200 Z" />
+    </Canvas>
+  )
+}
